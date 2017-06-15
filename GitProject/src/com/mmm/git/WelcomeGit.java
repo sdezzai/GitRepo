06696919a772b1da1cad4c22e25645843bd889de
sdezzai <shilpadesai21012@gmail.com>
2017-06-15 16:23:03 +0530

    commit from eclipse by usr2

--- GitProject/src/com/mmm/git/WelcomeGit.java
+++ GitProject/src/com/mmm/git/WelcomeGit.java
@@ -3,7 +3,7 @@ package com.mmm.git;
 public class WelcomeGit {
 	
 	public static void main(String[] args) {
-		System.out.println("Page1 by desai");
+		System.out.println("Page1 by shilpa");
 	}
 
 }
